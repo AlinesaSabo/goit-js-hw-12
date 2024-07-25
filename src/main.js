52f40d216cef0b2ab1c9135bf316062455630a47
Alinesa <alinesa322@gmail.com>
2024-07-25 23:24:33 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 import { searchImagesByQuery } from './js/pixabay-api';
 import { renderImageSearch } from './js/render-functions';
 
